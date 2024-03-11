69bdc8c75c2e0f7ea3389f6a1dfe3b45ade9e611
stevygmillenium <stevyg2008@hotmail.com>
2024-03-11 08:46:20 -0500

    Update.

--- src/enc_dec/shift_c.java
+++ src/enc_dec/shift_c.java
@@ -8,6 +8,9 @@ public class shift_c {
 		conv_data=enc(data);
 		System.out.println(conv_data);
 		System.out.println(dec(conv_data));
+		conv_data=enc_dec(data, true);
+		System.out.println(conv_data);
+		System.out.println(enc_dec(conv_data, false));
 	}
 	public static String enc(String data) {
 		char[]dec_data=data.toCharArray(),conv=new char[dec_data.length];
@@ -26,6 +29,37 @@ public class shift_c {
 			i++;
 		}
 		return new String(conv);
+	}
+	public static String enc_dec(String data,boolean enc) {
+		char[]c_data,conv;
+		String s_out = null,s_in,rev;
+		StringBuilder sb;
+		int i = 0;
+		if(enc==true) {
+			c_data=data.toCharArray();
+			conv=new char[c_data.length];
+			for(char c:c_data) {
+				conv[i]=(char) (c+1);
+				i++;
+			}
+			sb=new StringBuilder(new String(conv));
+			rev=sb.reverse().toString();
+			s_out=rev;
+		}
+		else if(enc==false) {
+			s_in=data;
+			sb=new StringBuilder(s_in);
+			rev=sb.reverse().toString();
+			conv=rev.toCharArray();
+			c_data=new char[conv.length];
+			for(char c:conv) {
+				c_data[i]=(char) (c-1);
+				i++;
+			}
+			s_out=new String(c_data);
+		}
+		else {}
+		return s_out;
 	}
 
 }
